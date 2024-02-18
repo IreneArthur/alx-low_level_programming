afb02388de77edb3ec6f8d4a89d9baf30ea7651b
Arthur <shingiroirene@gmail.com>
2024-02-18 10:16:44 +0200

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - this is the main
+ *
+ * Return: alwways return 0 when succeded
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+
+	putchar('\n');
+	return (0);
+}
