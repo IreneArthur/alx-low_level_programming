13ea3facf47ea0240eaebfd42805e17a1d7c5646
Arthur <shingiroirene@gmail.com>
2024-02-18 14:05:27 +0200

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * _abs - print the absolute value of number checked
+ * @n: number to be checked
+ *
+ * Return: absolute value of n
+*/
+int _abs(int n)
+{
+	return (abs(n));
+}
