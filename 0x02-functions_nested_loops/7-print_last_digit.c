ee0466ea0ba37bc7e9a17329ac0ab07185236ebd
Arthur <shingiroirene@gmail.com>
2024-02-18 14:47:47 +0200

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - prints the last ligit of inputed number
+ * @n: number to be checked
+ *
+ * Return: Absolute value of the number
+*/
+int print_last_digit(int n)
+{
+	int last_digit = n%10;
+
+	printf("%d",last_digit);
+	return (last_digit);
+}
