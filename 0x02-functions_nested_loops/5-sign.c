4e3a850f8df4962ba89bceed4564015d60d747c4
Arthur <shingiroirene@gmail.com>
2024-02-18 11:47:40 +0200

    chechs sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_sign - Prints the sign of a number and returns its sign.
+ * @n: The number to check
+ *
+ * Description: This function prints the sign of the number 'n' and returns
+ *              its sign as follows:
+ *                  - Returns 1 and prints '+' if n is greater than zero.
+ *                  - Returns 0 and prints '0' if n is zero.
+ *                  - Returns -1 and prints '-' if n is less than zero.
+ *
+ * Return: 1 if 'n' is greater than zero, 0 if 'n' is zero, -1 if 'n' is
+ *         less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n < 0)
+	{
+		printf("-");
+		return (-1);
+	}
+	else
+	{
+		printf("0");
+		return (0);
+	}
+}
