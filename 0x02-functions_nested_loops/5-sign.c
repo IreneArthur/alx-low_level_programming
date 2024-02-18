d318b33317c01ca44e601d2a717010969c8d22db
Arthur <shingiroirene@gmail.com>
2024-02-18 13:40:59 +0200

    chechs sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,19 @@
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        putchar('+');
-        return 1;
-    }
-    else if (n < 0)
-    {
-        putchar('-');
-        return -1;
-    }
-    else
-    {
-        putchar('0');
-        return 0;
-    }
+	if (n > 0)
+	{
+		putchar('+');
+		return 1;
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return -1;
+	}
+	else
+	{
+		putchar('0');
+		return 0;
+	}
 }
