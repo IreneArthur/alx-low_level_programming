9134bcf3b2bf0bad31fa2d66d32a9a99d9580f01
Arthur <shingiroirene@gmail.com>
2024-02-18 11:07:01 +0200

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - Print alphabets on ten lines
+*/
+void print_alphabet_x10(void)
+{
+	int size = 0;
+
+	while(size <= 10)
+	{
+	char all = 'a';
+
+	while(all <= 'z')
+	{
+		_putchar(all);
+		all++;
+	}
+	_putchar('\n');
+	size++;
+	}
+}
