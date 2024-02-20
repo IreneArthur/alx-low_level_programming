19db7fb922920de33c5e1c143f803d4d67927e1a
Arthur <shingiroirene@gmail.com>
2024-02-20 10:11:13 +0200

    numbers that are multiplies of 5 and 3

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,16 +13,14 @@ int main(void)
 	int a = 0;
 	int sum = 0;
 
-	printf("The numbers that are multiplies of 3 and five are:");
 	while (a < 1024)
 	{
 		if (a % 3 == 0 || a % 5 == 0)
 		{
-			printf("%d, ", a);
 			sum += a;
 		}
 		a++;
 	}
-	printf("there sum is %d\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
