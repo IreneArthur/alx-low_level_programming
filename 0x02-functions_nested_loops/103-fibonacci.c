90139c79adb586b078d317e7b36bb6cfda0ce87b
Arthur <shingiroirene@gmail.com>
2024-03-02 15:58:16 +0200

    sum of fibbonaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This program calculates the sum of all even Fibonacci numbers
+ * up to 4,000,000 and prints the result.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	long int a = 1, b = 2, c;
+	long int sum = 2;
+	while (c <= 4000000)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c % 2 == 0)
+		{
+			sum += c;
+		}
+	}
+	printf("%ld", sum);
+	return(0);
+
+}
