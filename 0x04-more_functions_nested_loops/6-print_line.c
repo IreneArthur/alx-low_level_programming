5e20566902c38a5fee0681b5f1b38dc53987d331
Arthur <shingiroirene@gmail.com>
2024-03-03 22:21:47 +0200

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * This program demonstrates the usage of an if-else statement within a while loop.
+ * It prints whether each iteration of the loop reached the value 3 or not.
+ *
+ * Return: Always 0 (success)
+ */
 void print_line(int n)
 {
 	int i = 1;
