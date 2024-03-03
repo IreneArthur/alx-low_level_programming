eeb6d32939723650b85e55cb2c27e7a16615d584
Arthur <shingiroirene@gmail.com>
2024-03-03 22:24:02 +0200

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
- * main - Entry point of the program
- *
- * This program demonstrates the usage of an if-else statement within a while loop.
- * It prints whether each iteration of the loop reached the value 3 or not.
- *
- * Return: Always 0 (success)
+ * print_line - Print a line of underscores
+ * @n: Number of underscores to print
  */
+
+
 void print_line(int n)
 {
 	int i = 1;
