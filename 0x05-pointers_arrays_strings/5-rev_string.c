0bdd8ddaec8209b260b965030029a6200d22b12c
Arthur <shingiroirene@gmail.com>
2024-03-04 16:21:42 +0200

    reverse 1

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+void rev_string(char *s)
+{
+	int leng = strlen(s);
+	char b;
+	int i;
+
+	for (i = 0; i <= leng / 2; i++)
+	{
+		b = s[i];
+		s[i] = s[leng - i - 1];
+		s[leng - i - 1] = b;
+	}
+}
