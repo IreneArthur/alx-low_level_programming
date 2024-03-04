1bc5082c2214db21b9cd61f7b196364d654af19b
Arthur <shingiroirene@gmail.com>
2024-03-04 17:05:10 +0200

    print data of the array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_array - Prints elements of an array of integers
+ * @a: Pointer to the array
+ * @n: Number of elements to print
+ *
+ * This function takes an array of integers and the number of elements to prin
+ * It prints each element of the array followed by a comma and a space, except
+ * the last element. It does not print a newline character.
+ */
 void print_array(int *a, int n)
 {
 	int i;
