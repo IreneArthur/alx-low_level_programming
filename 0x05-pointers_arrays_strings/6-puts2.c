194c3d85aa324802ba77d39e1d9ad8636a3c4b24
Arthur <shingiroirene@gmail.com>
2024-03-04 16:35:14 +0200

    skip a character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+/**
+ * puts2 - Prints every other character of a string
+ * @str: Pointer to the string
+ */
 void puts2(char *str)
 {
 	int i;
