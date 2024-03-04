4d2a73405f3c688becc2b3e298f58d26c0785635
Arthur <shingiroirene@gmail.com>
2024-03-04 13:53:53 +0200

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Prints a string followed by a newline to stdout
+ * @str: Pointer to the string to be printed
+ *
+ * This function prints the string pointed to by 'str'
+ * to the standard output, followed by a newline character ('\n').
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
