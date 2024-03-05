5302bd38fd3b3ce06b0e25d6fe58ceab67cb0d42
Arthur <shingiroirene@gmail.com>
2024-03-05 18:12:53 +0200

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,11 @@
 #include "main.h"
 #include <string.h>
-/*
- * _strcat-Concatenates the source string (src) to the destination string(dest)
- * The resulting string is terminated with a null character.
- * Returns a pointer to the modified destination string (dest).
+/**
+ * _strcat - Concatenates the source string (src) to the destination string
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ *
+ * Return: Pointer to the modified destination string (dest)
  */
 char *_strcat(char *dest, char *src)
 {
