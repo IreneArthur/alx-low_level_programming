4cb62df5db77ab7b44dd238c2f45a29e7f1189c9
Arthur <shingiroirene@gmail.com>
2024-03-05 11:42:49 +0200

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,10 @@
 #include "main.h"
 #include <string.h>
 /*
- * Concatenates the source string (src) to the destination string (dest).
- * This function overwrites the terminating null byte ('\0')
- * at the end of dest with the first character of src,
- * and then adds a null-terminator at the end of the concatenated string.
- * The behavior is undefined if the destination array is not large enough
- * for the contents of both src and dest and the null-terminator.
- * Returns a pointer to the resulting string (dest).
+ * _strcat-Concatenates the source string (src) to the destination string(dest)
+ * The resulting string is terminated with a null character.
+ * Returns a pointer to the modified destination string (dest).
  */
-
 char *_strcat(char *dest, char *src)
 {
 	strcat(dest, src);
